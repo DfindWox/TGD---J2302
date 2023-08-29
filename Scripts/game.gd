1fe678f689540017ffe3a7b3e710ab77b0e9577f
Necco-Z <necco@outlook.com>
2023-08-28 23:05:33 -0300

    Reorganização geral do projeto

--- Scripts/game.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(_delta):
-	if  Input.is_action_just_pressed("quit"):
-		get_tree().quit()
-	elif  Input.is_action_just_pressed("reset"):
-		get_tree().reload_current_scene()
-
-
--- Scripts/hit_box.gd
+++
@@ -1,8 +0,0 @@
-class_name HitBox
-extends Area2D
-
-@onready var parent = get_owner()
-
-
-func _on_area_entered(area):
-	pass # Replace with function body.
